403f10084b184b81b7c9f98780f75eb99edb256c
Igor Sá <167645991+gogoiaba@users.noreply.github.com>
2024-05-25 14:52:00 -0300

    identifiquei as variaveis

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function comprar() {
+    let tipoIngresso = document.getElementById("tipo-ingresso");
+    let qdt = document.getElementById("qtd");
+
+    alert(tipoIngresso.value);
+    alert(qdt.value);
+}
+
+let qtdPista = document.getElementById("qtd-pista").value;
+let qtdSuperior = document.getElementById("qtd-superior").value;
+let qtdInferior = document.getElementById("qtd-inferior").value;
+
