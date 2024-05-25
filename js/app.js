b7365095aef3b22134a109b83b092e5f8db951ca
Igor Sá <167645991+gogoiaba@users.noreply.github.com>
2024-05-25 15:31:24 -0300

    desafio feito sem olhar a alura

--- js/app.js
+++ js/app.js
@@ -1,12 +1,41 @@
 function comprar() {
-    let tipoIngresso = document.getElementById("tipo-ingresso");
-    let qdt = document.getElementById("qtd");
+    let tipoIngresso = document.getElementById("tipo-ingresso").value;
+    let qtd = parseInt(document.getElementById("qtd").value);
 
-    alert(tipoIngresso.value);
-    alert(qdt.value);
+    if (tipoIngresso === "pista") {
+        let qtdPistaElement = document.getElementById("qtd-pista");
+        let qtdPista = parseInt(qtdPistaElement.textContent);
+        if (checarDisponiveis(qtdPista, qtd)) {
+            qtdPistaElement.textContent = qtdPista - qtd;
+        }
+    }
+
+    if (tipoIngresso === "inferior") {
+        let qtdInferiorElement = document.getElementById("qtd-inferior");
+        let qtdInferior = parseInt(qtdInferiorElement.textContent);
+        if (checarDisponiveis(qtdInferior, qtd)) {
+            qtdInferiorElement.textContent = qtdInferior - qtd;
+        }
+    }
+
+    if (tipoIngresso === "superior") {
+        let qtdSuperiorElement = document.getElementById("qtd-superior");
+        let qtdSuperior = parseInt(qtdSuperiorElement.textContent);
+        if (checarDisponiveis(qtdSuperior, qtd)) {
+            qtdSuperiorElement.textContent = qtdSuperior - qtd;
+        }
+    }
+
+    if (qtd < 0) {
+        alert("Não há mais ingressos disponíveis nessa seção");
+        return;
+    }
 }
 
-let qtdPista = document.getElementById("qtd-pista").value;
-let qtdSuperior = document.getElementById("qtd-superior").value;
-let qtdInferior = document.getElementById("qtd-inferior").value;
-
+function checarDisponiveis(tag, qtd) {
+    if (tag - qtd < 0) {
+        alert("Não há esta quatidade de ingressos disponíveis nessa seção.");
+        return false;
+    }
+    return true;
+}
